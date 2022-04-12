1545f035be0bf2e9fb27350b3f0d4b40ed4dcd91
Diego Vergara <71197696+Ninewinger@users.noreply.github.com>
2022-04-11 20:31:34 -0400

    asd

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,8 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
   return (
     //comentario de prueba.
+    //todavia no aprendo nada xD
+    
     <div className={styles.container}>
       <Head>
         <title>Create Next App</title>
